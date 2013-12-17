b13694eb182cfe34e0a7632094106d4403a7dbc5
jTinks <jTink010@gmail.com>
2013-12-17 11:45:05 -0800

    randomTests added

--- JavaPractice/src/randomTests.java
+++ JavaPractice/src/randomTests.java
@@ -1,17 +1,25 @@
 import java.util.Random;
+import javax.swing.*;
 
 
 public class randomTests {
 	
 	public static void main(String args[]){
 		
-		Random r1 = new Random();
+		//Random r1 = new Random();
 		
-		int randomInt = r1.nextInt(100);
+		//int randomInt = r1.nextInt(100);
 		
-		System.out.println(randomInt);
+		//System.out.println(randomInt);
 		
 		
+		String name = JOptionPane.showInputDialog("What is your name?");
+		
+		String input = JOptionPane.showInputDialog("How old are you?");
+		int age = Integer.parseInt(input);
+		
+		System.out.print("Hello, " + name + ". ");
+		System.out.println("Next year you will be " + (age+1));
 	}
 
 }
